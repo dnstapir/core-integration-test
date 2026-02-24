b2c10d60b42b4952effff92bbfdfa6885c6f3fe5
Leon <leon.fernandez@internetstiftelsen.se>
2026-02-24 14:42:41 +0100

    Also shut down containers after testing

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "### Setting up tests..."
-docker compose -f sut/docker-compose.yaml up
+docker compose -f sut/docker-compose.yaml up -d
 
 echo "### Running tests..."
 pytest
 echo "### Done running tests!"
+
+docker compose -f sut/docker-compose.yaml stop
