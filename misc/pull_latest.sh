90004e19d4ba950cf1f46f5f7215162b66225ca5
Leon <leon.fernandez@internetstiftelsen.se>
2026-07-15 11:56:11 +0200

    Convenience script to pull latest images for SUT

---
+++ misc/pull_latest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker pull ghcr.io/dnstapir/observation-encoder:latest
+docker pull ghcr.io/dnstapir/tapir-analyse-listchecker:latest
+docker pull ghcr.io/dnstapir/tapir-analyse-looptest:latest
+docker pull ghcr.io/dnstapir/tapir-analyse-new-qname:latest
